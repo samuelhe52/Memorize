8117a8a7d84c148ace288fd3940f7dc0afca08f5
samuelhe864 <samuelhe52@outlook.com>
2024-04-27 13:31:15 +0800

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Samuel He on 2024/4/27.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
