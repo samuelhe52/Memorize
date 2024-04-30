b77feb361444dce921b7edf4dd2d0848ab031f8f
samuelhe864 <samuelhe52@outlook.com>
2024-04-30 20:09:46 +0800

    CardView Flip on change

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -10,12 +10,38 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            ForEach(0..<5) { _ in
+                HStack {
+                    CardView(isFaceUp: true)
+                    ForEach(0..<3) { _ in
+                        CardView()
+                    }
+                }
+                .padding()
+            }
         }
         .padding()
+        .foregroundStyle(.orange)
+    }
+}
+
+struct CardView: View {
+    @State var isFaceUp = false // Make isFaceUp mutable.
+    
+    var body: some View {
+        ZStack {
+            let base = RoundedRectangle(cornerRadius: 25)
+            if isFaceUp {
+                base.foregroundStyle(.white)
+                base.strokeBorder(lineWidth: 3)
+                Text("👻").font(.largeTitle)
+            } else {
+                base
+            }
+        }
+        .onTapGesture {
+            isFaceUp.toggle()
+        }
     }
 }
 
