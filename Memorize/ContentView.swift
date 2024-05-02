945f4d1b992d97f266533bf4a2c12f2426189301
samuelhe864 <samuelhe52@outlook.com>
2024-05-02 22:29:46 +0800

    Added more details into the themes Now themes come with thier own colors

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -7,94 +7,82 @@
 
 import SwiftUI
 
-let halloweenTheme: [String] = ["👻", "🎃", "💀", "🕷️", "👿", "🕸️", "🐊", "🐙", "😱", "🍭", "🧙", "🙀"]
-let animalTheme: [String] = ["🐶", "🐭", "🐹", "🐰", "🦊", "🐻", "🐼", "🐻‍❄️", "🐯", "🦁", "🐮", "🐷"]
-let digitalTheme: [String] = ["⌚️", "📱", "💻", "⌨️", "🖥️", "🖨️", "💿", "💾", "📷", "☎️", "📺", "📡"]
+let halloweenTheme: [String] = ["👻", "🎃", "💀", "🕷️", "👿", "🕸️"]
+let animalTheme: [String] = ["🐶", "🐭", "🐹", "🐰", "🦊", "🐻"]
+let digitalTheme: [String] = ["⌚️", "📱", "💻", "⌨️", "🖥️", "🖨️"]
+let availableThemes: [String: (Color, [String])] = ["Halloween": (.purple, halloweenTheme), "Animal": (.pink, animalTheme), "Digital": (.blue, digitalTheme)]
 
 struct ContentView: View {
-    @State var currentTheme: [String] = halloweenTheme
-    @State var cardCount: Int = 5
+    @State var currentTheme: [String] = halloweenTheme.flatMap { [$0, $0] }.shuffled()
+    @State var currentColor: Color = .cyan
     
     var body: some View {
         VStack {
-            Text("Memorize!")
-                .font(.largeTitle)
-                .foregroundStyle(.linearGradient(colors: [.pink, .purple, .blue], startPoint: .topLeading, endPoint: .bottomTrailing))
-            themeModifiers
+            title
+            Spacer(minLength: 20)
             ScrollView {
-                cards
+                cards.foregroundStyle(currentColor)
             }
-            Spacer()
-            cardCountModifiers
+            themeModifiers
         }
         .padding()
     }
     
+    var title: some View {
+        Text("Memorize!")
+            .font(.largeTitle)
+            .foregroundStyle(.linearGradient(colors: [.pink, .purple, .blue], startPoint: .topLeading, endPoint: .bottomTrailing))
+    }
+    
     var cards: some View {
         LazyVGrid(columns: [GridItem(.adaptive(minimum: 80))]) {
-            ForEach(0..<cardCount, id: \.self) { index in
+            ForEach(0..<currentTheme.count, id: \.self) { index in
                 CardView(content: currentTheme[index])
                     .aspectRatio(2/3, contentMode: .fit)
             }
         }
-        .foregroundStyle(.orange)
-    }
-    
-    var cardCountModifiers: some View {
-        HStack {
-            cardRemover
-            Spacer()
-            cardAdder
-        }
-        .imageScale(.large)
     }
     
     var themeModifiers: some View {
-        VStack {
-            Text("Choose your theme here")
-            HStack {
-                Button("Halloween") {
-                    currentTheme = halloweenTheme
-                }
-                Button("Animal") {
-                    currentTheme = animalTheme
-                }
-                Button("Digital") {
-                    currentTheme = digitalTheme
-                }
-            }
+        HStack(alignment: .lastTextBaseline, spacing: 35) {
+            themeModifier(to: "Animal", symbol: "dog")
+            themeModifier(to: "Halloween", symbol: "ant")
+            themeModifier(to: "Digital", symbol: "pc")
         }
     }
     
-    func cardCountModifier(by offset: Int, symbol: String) -> some View {
-        Button(action: {
-            cardCount += offset
-        }, label: {
-            Image(systemName: symbol)
-        })
-        .disabled(cardCount + offset < 1 || cardCount + offset > currentTheme.count)
+    func themeProcessor(_ theme: [String]) -> [String] {
+        return (theme + theme).shuffled()
     }
     
-    var cardRemover: some View {
-        cardCountModifier(by: -1, symbol: "rectangle.stack.badge.minus")
-    }
-    
-    var cardAdder: some View {
-        cardCountModifier(by: 1, symbol: "rectangle.stack.badge.plus")
+    func themeModifier(to theme: String, symbol: String) -> some View {
+        let themeEmojis = themeProcessor(availableThemes[theme]?.1 ?? halloweenTheme)
+        let themeColor = availableThemes[theme]?.0 ?? .purple
+        return VStack {
+            Button(action: {
+                currentTheme = themeEmojis
+                currentColor = themeColor
+            }, label: {
+                Image(systemName: symbol)
+                    .font(.title2)
+            })
+            Text(theme)
+                .font(.footnote)
+        }
+        .foregroundStyle(themeColor)
     }
 }
 
 struct CardView: View {
     let content: String
-    @State var isFaceUp = true // Make isFaceUp mutable.
+    @State var isFaceUp = false // Make isFaceUp mutable.
     
     var body: some View {
         ZStack {
             let base = RoundedRectangle(cornerRadius: 15)
             base.strokeBorder(lineWidth: 3)
             Text(content).font(.largeTitle)
-            base.foregroundStyle(.orange)
-                .opacity(isFaceUp ? 0 : 1)
+            base.opacity(isFaceUp ? 0 : 1)
         }
         .onTapGesture {
             isFaceUp.toggle()
