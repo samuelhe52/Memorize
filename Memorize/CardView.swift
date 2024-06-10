f377cb57fa6c9f5b7201eaff2db65a83fbaf9fac
samuelhe864 <samuelhe52@outlook.com>
2024-06-10 12:07:03 +0800

    Added preview for CardView

--- Memorize/CardView.swift
+++ Memorize/CardView.swift
@@ -8,7 +8,9 @@
 import SwiftUI
 
 struct CardView: View {
-    let card: MemoryGame<String>.Card
+    typealias Card = MemoryGame<String>.Card
+
+    let card: Card
     let baseColor: Color
     
     var body: some View {
@@ -17,8 +19,10 @@ struct CardView: View {
             base.strokeBorder(lineWidth: 3)
             Text(card.content)
                 .font(.system(size: 200))
-                .minimumScaleFactor(0.01)
+                .minimumScaleFactor(0.1)
+                .multilineTextAlignment(.center)
                 .aspectRatio(4/3, contentMode: .fit)
+                .padding(5)
             base.opacity(card.isFaceUp ? 0 : 1)
         }
         .foregroundStyle(.linearGradient(stops: baseColor.brightnessGradient.stops, startPoint: .bottomLeading, endPoint: .topTrailing))
@@ -26,4 +30,21 @@ struct CardView: View {
         .animation(.easeInOut(duration: 0.5), value: card.isMatched)
         .animation(.easeInOut(duration: 0.2), value: card.isFaceUp)
     }
+}
+
+#Preview {
+    typealias Card = MemoryGame<String>.Card
+    typealias CardID = MemoryGame<String>.CardID
+    
+    return VStack {
+        HStack {
+            CardView(card: Card(isFaceUp: true, content: "X", id: CardID(description: "test1")), baseColor: .blue)
+            CardView(card: Card(content: "X", id: CardID(description: "test1")), baseColor: .blue)
+        }
+        HStack {
+            CardView(card: Card(isFaceUp: true, content: "This is a very long string and I hope it fits.", id: CardID(description: "test1")), baseColor: .blue)
+            CardView(card: Card(isMatched: true, content: "X", id: CardID(description: "test1")), baseColor: .blue)
+        }
+    }
+    .padding()
 }
